d5af040ba0b97bdf1fc56f874437c7472931145a
Thomas Guzman <tg@resolving.com>
2019-03-03 11:37:32 +0100

    Improve error handling

--- src/SQLite.NetStandard/Helper.cs
+++ src/SQLite.NetStandard/Helper.cs
@@ -7,12 +7,15 @@ namespace SQLite {
 	
     public static class Helper {
         internal static void ThrowIfNeeded(sqlite3 db, int result, int expected, string prefix) {
-            if (db != null && result == (int)Error.Error) {
-                var err = sqlite3_errmsg(db);
-                throw new SQLiteException($"{prefix} : {err}");
-            }
-            if (result != expected) {
-                throw new SQLiteException($"{prefix}: {result}");
+            if (db != null && result != expected) {
+                
+                string err = null;
+                try {
+                    err = sqlite3_errmsg(db);
+                }
+                catch { }
+
+                throw new SQLiteException($"{prefix} : {result} ({(Error)result}) : {err ?? "No error message"}");
             }
         }
     }
